64bfc4e5f7f3d61f4875f327d52a3b563d534823
SL <slyoshin@icloud.com>
2021-07-28 11:18:48 +0500

    lesson_6

---
+++ lesson_6/station.rb
@@ -0,0 +1,46 @@
+require "./instances"
+require "./validation_error"
+
+class Station
+  include Instances
+  include InstanceCounter
+  attr_reader :name, :trains
+
+  NAME_PATTERN = /^[a-z]{3,}$/i
+
+  def all
+    self.instances
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    validate!
+    add(self)
+  end
+
+  def take(train)
+    trains << train
+  end
+
+  def send_out(train)
+    trains.delete(train)
+  end
+
+  def trains_with_type(type)
+    trains.select { |train| train.type == type }
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  protected
+
+  def validate!
+    raise ValidationError, "Name must contains 3 letters a-z or more" if name !~ NAME_PATTERN
+    true
+  end
+end
